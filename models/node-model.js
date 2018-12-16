3a5c9019f54af5d3173b987f6cc2e3c0dc0e0b6c
saibot777 <stefantrajkovic777@gmail.com>
2018-12-16 01:23:55 +0100

    model created

---
+++ models/node-model.js
@@ -0,0 +1,8 @@
+class Block {
+    constructor({ timestamp, lastHash, hash, data }) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+}
