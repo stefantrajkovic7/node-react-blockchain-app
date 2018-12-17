a221284e1c9f5ed6eeff7ebb6f99412440ed6647
saibot777 <stefantrajkovic777@gmail.com>
2018-12-17 15:59:01 +0100

    created crypto method

--- core/crypto-hash.js
+++ core/crypto-hash.js
@@ -1,5 +1,13 @@
-const cryptoHash = () => {
-    
+const crypto = require('crypto');
+
+const cryptoHash = (...inputs) => {
+    const hash = crypto.createHash('sha256');
+
+    hash.update(inputs
+        .sort()
+        .join(' '));
+
+    return hash.digest('hex');
 }
 
 module.exports = cryptoHash
