45cf3dc57c646d336e2774319634644009dba96d
saibot777 <stefantrajkovic777@gmail.com>
2018-12-25 00:13:32 +0100

    overrall refactoring and syncing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,7 +8,10 @@ const PubSub = require('./core/pubsub');
 
 const app = express();
 
+const DEFAULT_PORT = 5000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 const UI_API_URL = 'http://localhost:3000';
+
 
 // Express Configuration
 app.use(morgan('dev'));
@@ -34,4 +38,31 @@ setTimeout(() => pubsub.broadcast(), 1000);
 // API Routes
 require('./routes')(app);
 
-module.exports = app;
+// Server
+
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/v1/blocks`}, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const rootChain = JSON.stringify(body);
+
+            console.log('replace chain on a sync with', rootChain)
+
+            blockchain.replaceChain(rootChain);
+        }
+    });
+}
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000)
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+app.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}`);
+
+    syncChains();
+});
+
