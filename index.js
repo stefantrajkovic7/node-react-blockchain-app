767e7356d0ae07076dc68c6724ca447ef1034862
saibot777 <stefantrajkovic777@gmail.com>
2018-12-24 23:48:22 +0100

    generating random ports func and command impl

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const app = require('./app');
 
-const port = process.env.PORT || 5000;
+const DEFAULT_PORT = 5000;
+let PEER_PORT;
 
-app.listen(port, (err) => {
+if (process.env.GENERATE_PEER_PORT === 'true') {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000)
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+app.listen(PORT, (err) => {
     if (err) {
         return console.log(err)
     }
 
-    return console.log(`server is listening on ${port}`)
+    return console.log(`server is listening on ${PORT}`)
 });
