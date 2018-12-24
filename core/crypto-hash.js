5b5f1557929a67e7abdb83f0df61a2ccce232339
saibot777 <stefantrajkovic777@gmail.com>
2018-12-24 12:45:37 +0100

    increasing difficulty with hex transf

--- core/crypto-hash.js
+++ core/crypto-hash.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const hexToBinary = require('hex-to-binary');
 
 const cryptoHash = (...inputs) => {
     const hash = crypto.createHash('sha256');
@@ -7,7 +8,7 @@ const cryptoHash = (...inputs) => {
         .sort()
         .join(' '));
 
-    return hash.digest('hex');
+    return hexToBinary(hash.digest('hex'));
 }
 
 module.exports = cryptoHash
