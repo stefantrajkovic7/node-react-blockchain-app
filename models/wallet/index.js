1c46d2e2c81a90e844f0f421f51928813f85ee5f
saibot777 <stefantrajkovic777@gmail.com>
2018-12-27 19:44:38 +0100

    signature func impl

--- models/wallet/index.js
+++ models/wallet/index.js
@@ -1,13 +1,18 @@
 const { STARTING_BALANCE } = require('../../data');
 const { ec }  = require('../../utils/elliptic');
+const cryptoHash = require('../../utils/crypto-hash');
  
 class Wallet {
     constructor() {
         this.balance = STARTING_BALANCE;
 
-        const keyPair = ec.genKeyPair();
+        this.keyPair = ec.genKeyPair();
 
-        this.publicKey = keyPair.getPublic().encode('hex');
+        this.publicKey = this.keyPair.getPublic().encode('hex');
+    }
+
+    sign(data) {
+        return this.keyPair.sign(cryptoHash(data));
     }
 }
 
