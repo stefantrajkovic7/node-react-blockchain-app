fd2232b56a74d5daaeda605251b675e3bf0f8764
saibot777 <stefantrajkovic777@gmail.com>
2018-12-27 03:04:42 +0100

    wallet initiation func impl

--- models/wallet/index.js
+++ models/wallet/index.js
@@ -1,7 +1,14 @@
 const { STARTING_BALANCE } = require('../../data');
-
+const { ec }  = require('../../utils/elliptic');
+ 
 class Wallet {
+    constructor() {
+        this.balance = STARTING_BALANCE;
 
+        const keyPair = ec.genKeyPair();
+
+        this.publicKey = keyPair.getPublic();
+    }
 }
 
 module.exports = Wallet;
