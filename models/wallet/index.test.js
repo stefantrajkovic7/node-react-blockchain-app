6b84535a804318041bb4d3db205fc0fb7bb93729
saibot777 <stefantrajkovic777@gmail.com>
2018-12-27 18:58:54 +0100

    signature test impl

--- models/wallet/index.test.js
+++ models/wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('.');
+const { verifySignature } = require('../../utils/elliptic');
 
 describe('Wallet', () => {
     let wallet;
@@ -14,4 +15,32 @@ describe('Wallet', () => {
     it('has a `publicKey`', () => {
         expect(wallet).toHaveProperty('publicKey');
     });
+
+    describe('signing data', () => {
+        const data = 'foobar';
+
+        it('verifies a signature', () => {
+
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: wallet.sign(data)
+                })
+            ).toBe(true)
+            
+        });
+
+        it('does not verify a invalid signature', () => {
+
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: new Wallet().sign(data)
+                })
+            ).toBe(false)
+
+        });
+    })
 });
