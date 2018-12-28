6f85eaee13188de7b2156298f957531eb29d9581
saibot777 <stefantrajkovic777@gmail.com>
2018-12-28 04:10:49 +0100

    wallet createTransaction() test impl

--- models/wallet/index.test.js
+++ models/wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('.');
+const Transaction = require('./transaction');
 const { verifySignature } = require('../../utils/elliptic');
 
 describe('Wallet', () => {
@@ -42,5 +43,36 @@ describe('Wallet', () => {
             ).toBe(false)
 
         });
-    })
+    });
+
+    describe('createTransaction()', () => {
+        describe('and the amount exceeds the balance', () => {
+            it('throws an error', () => {
+                expect(() => wallet.createTransaction({ amount: 999999, recipient: 'foo-recipient' }))
+                    .toThrow('Amount exceeds balance');
+            });
+        });
+
+        describe('and the amount is valid', () => {
+            let transaction, amount, recipient;
+
+            beforeEach(() => {
+                amount = 50;
+                recipient = 'foo-recipient';
+                transaction = wallet.createTransaction({ amount, recipient })
+            })
+
+            it('creates an instance of `Transaction`', () => {
+                expect(transaction instanceof Transaction).toBe(true);
+            });
+
+            it('matches the transaction input of the wallet', () => {
+                expect(transaction.input.address).toEqual(wallet.publicKey);
+            });
+
+            it('outputs the amount of the recipient', () => {
+                expect(transaction.outputMap[recipient]).toEqual(amount);
+            });
+        });
+    });
 });
