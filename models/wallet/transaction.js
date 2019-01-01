096ebe0a1d0633bd26f522e62bbcf678a0959f9a
saibot777 <stefantrajkovic777@gmail.com>
2019-01-01 16:27:48 +0100

    transaction update() func update

--- models/wallet/transaction.js
+++ models/wallet/transaction.js
@@ -27,6 +27,10 @@ class Transaction {
     }
 
     update({ senderWallet, recipient, amount }) {
+        if (amount > this.outputMap[senderWallet.publicKey]) {
+            throw new Error('Amount exceeds balance');
+        }
+
         this.outputMap[recipient] = amount;
 
         this.outputMap[senderWallet.publicKey] = this.outputMap[senderWallet.publicKey] - amount;
