8fb913df6645671ad5261c2cff5c249cc39c9b59
saibot777 <stefantrajkovic777@gmail.com>
2019-01-04 21:42:19 +0100

    transaction miner initiated

---
+++ utils/transaction-miner.js
@@ -0,0 +1,13 @@
+class TransactionMiner {
+    mineTransaction() {
+        // get the transaction pool's valid transaction
+
+        // generate the miner's reward
+
+        // add a block consisting of these transactions to the blockchain
+
+        // broadcasting
+    }
+}
+
+module.exports = TransactionMiner;
